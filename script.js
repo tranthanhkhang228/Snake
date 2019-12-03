cad3e9d2ff14bd488ac77ed5a9db4523846d10a4
tranthanhkhang2281999 <45063079+tranthanhkhang2281999@users.noreply.github.com>
2019-12-03 19:18:56 +0700

    Add Wall and Bait

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const body = document.querySelector('body');
 const gameFrame = document.querySelector('.game-frame');
 
+const bait = document.createElement('div');
+bait.setAttribute('class', 'bait');
+gameFrame.append(bait);
+
 const template = /\d/g;
 
 let soDot = 3;
 let ran = [];
+let baitCoordinate = {
+    x: 0,
+    y: 0
+};
 
 const createNode = (x, y) => {
     const nodeSnake = document.createElement('div');
@@ -56,18 +64,39 @@ const move = (direct) => {
 let checkHead = () => {
     let x = convert(ran[0].style.left);
     let y = convert(ran[0].style.top);
-    if (x < 0 | x > 48 | y < 0 | y > 48) {
+
+    if (x < 2 | x > 50 | y < 2 | y > 50) {
+        body.removeEventListener('keyup', call);
         clearInterval(play);
     }
 }
 
+const displayBait = () => {
+    const x = Math.floor(Math.random() * 48) + 2;
+    const y = Math.floor(Math.random() * 48) + 2;
+    
+    baitCoordinate.x = (x % 2 === 0) ? x : x - 1;
+    baitCoordinate.y = (y % 2 === 0) ? y : y - 1;
 
+    bait.style.left = `${baitCoordinate.x}rem`;
+    bait.style.top = `${baitCoordinate.y}rem`;
+}
+
+const eat = () => {
+    let x = convert(ran[0].style.left);
+    let y = convert(ran[0].style.top);
+
+    if (x === baitCoordinate.x && y === baitCoordinate.y) {
+        displayBait();
+    }
+}
 
 initialize();
+displayBait();
 
 let direct = 'UP';
 
-body.addEventListener('keyup', e => {
+const call = (e) => {
     switch (e.keyCode) {
         case 37:
             direct = 'LEFT';
@@ -82,11 +111,14 @@ body.addEventListener('keyup', e => {
             direct = 'DOWN';
             break;
     };
-})
+};
+
+body.addEventListener('keyup', call);
 
 let play = setInterval(() => {
     move(direct);
     checkHead();
+    eat();
 }, 200);
 
 
